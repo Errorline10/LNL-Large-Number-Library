afe09156f0a403c6a1498df71f6a003f248effcd
MichaelBoston <104586300+Errorline10@users.noreply.github.com>
2025-04-03 01:14:06 -0400

    LNL-first-working-autocomplete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LNL-Large-Number-Library
  math operators for numbers with more than 22 digits
+
+# word list source
+https://github.com/dwyl/english-words/blob/master/words.txt
--- javascript/autocomplete.js
+++ javascript/autocomplete.js
@@ -4,12 +4,15 @@ const _autocomplete = () => {
   let domId = "";
   let array = [];
   let recordedFirstLetter = "1";
+  let currentFocus = 1;
 
   // public functions
   function _init(x) {
     this.domId = x;
     this.el = document.getElementById(this.domId);
-    this.listenIn("hello");
+    this.currentFocus = 1;
+    this.listenIn();
+    this.setKeyListener();
   }
 
   function _listenIn(e) {
@@ -22,7 +25,7 @@ const _autocomplete = () => {
       this.loadList();
     }
 
-    if (this.el.value.length > 3) {
+    if ((this.el.value.length > 1) && (e.keyCode !== 13)) {
       this.createSuggestionsDom();
     }
   }
@@ -46,20 +49,23 @@ const _autocomplete = () => {
     autocompleteList.setAttribute("class", "autocomplete-items");
     this.el.parentNode.appendChild(autocompleteList);
 
+    let displayPlace = 0;
+    if (this.currentFocus === 0) {
+      this.currentFocus = 1;
+    }
+    console.log(this.currentFocus)
     for (let i = 0; i < this.array.length; i++) {
       if (
         this.array[i].substr(0, this.el.value.length).toUpperCase() ==
         this.el.value.toUpperCase()
       ) {
+        displayPlace++;
         let item = document.createElement("DIV");
-        item.innerHTML =
-          "<strong>" +
-          this.array[i].substr(0, this.el.value.length) +
-          "</strong>";
+        if (this.currentFocus == displayPlace) { item.classList.add('selected') }
+        item.innerHTML = "<strong>" + this.array[i].substr(0, this.el.value.length) + "</strong>";
         item.innerHTML += this.array[i].substr(this.el.value.length);
         item.innerHTML += "<input type='hidden' value='" + this.array[i] + "'>";
         // add click event to list item
-        let x = "mike";
         item.addEventListener("click", (e) => {
           this.el.value = e.target.getElementsByTagName("input")[0].value;
           this.clearSuggestions();
@@ -77,6 +83,32 @@ const _autocomplete = () => {
     }
   }
 
+  function _setKeyListener() {
+    // keyboard navigation of the displayed suggestions
+    this.el.addEventListener("keydown", (e) => {
+      let x = document.getElementById(this.domId + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      switch (e.keyCode) {
+        case 40: // down key
+          this.currentFocus++;
+          break;
+        case 38: // up key
+          this.currentFocus--;
+          break;
+        case 13: // enter key
+          e.preventDefault();
+          if (this.currentFocus > -1) {
+            if (x) x[this.currentFocus-1].click();
+          }
+          break;
+        default: // all other keys
+          this.currentFocus = 0;
+          break;
+      }
+    });
+  }
+
+
   return {
     init: _init,
     keyed: _keyed,
@@ -84,21 +116,7 @@ const _autocomplete = () => {
     createSuggestionsDom: _createSuggestionsDom,
     loadList: _loadList,
     clearSuggestions: _clearSuggestions,
+    setKeyListener: _setKeyListener
   };
 };
 export default _autocomplete;
-
-// private functions
-// async function load(callback) {
-//   import("/lookupTables/englishDictionary/a.js")
-//     .then((module) => {
-//       console.log("module loaded");
-//       let a = module.default;
-//       console.log(a.data[10]);
-
-//       callback(a);
-//     })
-//     .catch((err) => {
-//       console.log("error loading module");
-//     });
-// }
